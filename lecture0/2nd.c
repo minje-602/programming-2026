ac4e35f0b295a55637a3a88bc422ff203dbebb26
김민제 <a98103022@gmail.com>
2026-03-06 04:50:33 +0000

    Add Hello World program with comments(comment practice)

---
+++ lecture0/2nd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+   //간단한 Hello World 출력
+    printf("Hello, World!\n");
+    /*
+    여러 부분에 주석 삽입 가능
+    여러 줄의 주석을 넣을 수 있습니다
+    포스트잇과 같은 역할
+    */
+    
+    
+    
+    return 0;
+}
